a1b1f1a7e0688f3d79a35e09c396415f45c1edb1
ramborell23 <jerelldavis@ac.c4q.nyc>
2019-02-27 00:20:04 -0500

    New branch checkout

---
+++ whats-on/src/components/MovieItem.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+    <!-- <p>Name: {{ coin.name }}</p>
+    <p>Symbol: {{ coin.symbol }}</p>
+    <p>Price (USD): {{ coin.price_usd }}</p>
+    <br/> -->
+    <br/>
+    
+    {{movie.title }}<br/>
+                Rating: {{movie.vote_average }}<br/>
+                Release Date: {{movie.release_date}}<br/>
+                Budget: {{movie.budget}}<br/>
+    <img v-bind:src="'http://image.tmdb.org/t/p/w92/'+movie.poster_path" alt='movie poster'/>
+    <!-- {{console.log("movie_image")}} -->
+    <br/>
+    <router-link :to="{ name: 'home' }">Back</router-link>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'MoviesItems',
+    data() {
+        return {
+            movie: {},
+            movie_video:{}
+        }
+    },
+
+    created() {
+        this.mounted()
+    },
+
+    watch: {
+        '$route': 'mounted'
+    },
+
+    methods: {
+        mounted() {
+            //   console.log(this.$route.params.id)
+            let movieItemId = this.$route.params.movieid
+            axios
+                .get(`https://api.themoviedb.org/3/movie/${movieItemId}?api_key=ff219f24cf866a850c34d6a49bdaf425&language=en-US`)
+                .then((resp) => {
+                    this.movie = resp.data
+                    console.log("MOie==>", this.movie)
+                    // console.log("MOie==>", this.$route.params.movieid)
+                    axios
+                .get(`https://api.themoviedb.org/3/movie/${movieItemId}/videos?api_key=ff219f24cf866a850c34d6a49bdaf425&language=en-US`)
+                .then((resp) => {
+                    this.movie_video = resp.data
+                    console.log("movie_video==>", this.movie_video)
+                    // console.log("MOie==>", this.$route.params.movieid)
+                })
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    }
+}
+</script>
--- whats-on/src/router.js
+++ whats-on/src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Test from './components/Test.vue'
+import Movies from './views/Movies.vue'
+import MovieItem from './components/MovieItem.vue'
 
 Vue.use(Router)
 
@@ -14,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/movies',
+      name: 'movies',
+      component: Movies,
+    },
+    {
+      path: '/movies/:movieid',
+      name: 'MoviesItems',
+      component: MovieItem,
     },
     {
       path: '/about',
